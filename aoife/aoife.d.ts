bfe5abd441744d4848301452b80f973b25911448
pillar <ymblender@gamil.com>
2021-01-18 13:15:54 +0800

    feat: remove module *

--- aoife/aoife.d.ts
+++ aoife/aoife.d.ts
@@ -1,59 +1,49 @@
 /// <reference path="./lib/interface.d.ts" />
 
-declare module "*.css" {
-  const content: string;
-  export default content;
-}
+// declare module "*.css" {
+//   const content: string;
+//   export default content;
+// }
 
-declare module "*.less" {
-  const content: string;
-  export default content;
-}
+// declare module "*.less" {
+//   const content: string;
+//   export default content;
+// }
 
-declare module "*.sass" {
-  const content: string;
-  export default content;
-}
+// declare module "*.sass" {
+//   const content: string;
+//   export default content;
+// }
 
-declare module "*.md" {
-  const content: string;
-  export default content;
-}
+// declare module "*.md" {
+//   const content: string;
+//   export default content;
+// }
 
-declare module "*.text" {
-  const content: string;
-  export default content;
-}
+// declare module "*.text" {
+//   const content: string;
+//   export default content;
+// }
 
-declare module "object-hash" {
-  const content: any;
-  export default content;
-}
+// declare module "@babel/*" {
+//   const content: any;
+//   export default content;
+// }
 
-declare module "@babel/*" {
-  const content: any;
-  export default content;
-}
+// declare module "@babel/standalone" {
+//   const content: any;
+//   export default content;
+// }
 
-declare module "@babel/core" {
-  const content: any;
-  export default content;
-}
+// declare module "@babel/preset-react" {
+//   const content: any;
+//   export default content;
+// }
 
-declare module "@babel/standalone" {
-  const content: any;
-  export default content;
-}
-
-declare module "@babel/preset-react" {
-  const content: any;
-  export default content;
-}
-
-declare module "babel-preset-minify" {
-  const content: any;
-  export default content;
-}
+// declare module "babel-preset-minify" {
+//   const content: any;
+//   export default content;
+// }
 
 import aoife from "./lib";
 
