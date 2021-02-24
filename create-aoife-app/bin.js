cc6478397a48fa691a221c765d691f3781908de3
pillar <ymblender@gamil.com>
2021-02-24 18:12:01 +0800

    feat: aofie 默认使用 webpack

--- create-aoife-app/bin.js
+++ create-aoife-app/bin.js
@@ -5,7 +5,7 @@ const resolve = require("path").resolve;
 const pwd = (...args) => resolve(process.cwd(), ...args);
 const argv = process.argv.splice(2);
 
-const build = argv[1] === "--webpack" ? "webpack" : "vite";
+const build = argv[1] === "--vite" ? "vite" : "webpack";
 
 fs.copySync(resolve(__dirname, build), pwd(argv[0]));
 fs.copyFile(resolve(__dirname, "gitignore"), pwd(argv[0], ".gitignore"));
