9c07ba334c294f43269d95b004591b19812f6e96
pillar <ymblender@gamil.com>
2021-05-05 11:50:08 +0800

    fix: change d.ts

--- aoife/lib/interface.d.ts
+++ aoife/lib/interface.d.ts
@@ -2,9 +2,17 @@ type PartialDetail<T> = {
   [P in keyof T]?: Partial<T[P]> | (() => Partial<T[P]>);
 };
 
-type PartialJSX<T> = {
-  [P in keyof T]?: PartialDetail<T[P]>;
-};
+type PartialJSX<T> =
+  | {
+      [P in keyof T]?: PartialDetail<T[P]>;
+    }
+  | {
+      [E in keyof ExtendHTML]?: ExtendHTML<E>;
+    };
+
+// type PartialJSXCustom<T> = {
+//   [E in keyof ExtendHTML]?: ExtendHTML<E>;
+// };
 
 type JSXHTML<T> = {
   [P in keyof HTMLElementTagNameMap]?: Partial<HTMLElementTagNameMap[P]>;
@@ -12,24 +20,33 @@ type JSXHTML<T> = {
 
 type RefOne = (e: HTMLInputElement) => any;
 
-interface ExtendHTML {
+type ExtendHTML = {
   class?: any;
   ref?: RefOne;
   /** 当元素更新时执行 */
-  watch?: () => any;
+  watch?: RefOne;
   /** 当元素 append 时回调 */
-  onAppend?: () => any;
+  onAppend?: RefOne;
   /** 当元素 remove 时回调 */
-  onRemove?: () => any;
+  onRemove?: RefOne;
   /** 当元素 entry 时回调 */
   onEntry?: any;
-}
+  children?: any[] | (() => any[]);
+  [key: string]: any;
+};
 
-type IProps = PartialJSX<HTMLInputElement> & ExtendHTML;
-type IPropsBy<K extends keyof HTMLElementTagNameMap> = PartialJSX<HTMLElementTagNameMap[K]> & ExtendHTML;
+// type IProps =
+// interface IProps extends PartialJSX<HTMLInputElement>, ExtendHTML {}
+
+// 标准元素列表
+type AoifeJSX = PartialJSX<HTMLElementTagNameMap>;
+interface IProps extends PartialDetail<HTMLInputElement>, ExtendHTML {}
 
 declare namespace JSX {
-  interface IntrinsicElements extends PartialJSX<HTMLElementTagNameMap> {}
+  interface IntrinsicElements extends AoifeJSX {
+    // 标准元素之外的元素
+    [key: string]: IProps;
+  }
 }
 
 declare const aoife: {
